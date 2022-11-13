33aa727e31a5becc8fdf537f815b637662bf14a3
Kirill <ForStmKar@gmail.com>
2022-11-13 13:52:21 +0300

    shapearea

---
+++ Задание 1/1/ShapeArea/ShapeArea/ShapeArea.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    internal class ShapeArea
+    {
+double TriangleArea(int a, int b, int c){
+
+
+if((a + b > c)&(a + c > b)&(c + b > a)){
+
+double p = (a + b + c)/2;
+double S = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+Console.WriteLine("Площадь треугольника: " + S);
+return S;
+}
+else Console.WriteLine("Такого треугольника не существует!");
+return a&b&c;
+}
+
+double RoundArea(int r){
+    double S = Math.PI * r*r;
+    System.Console.WriteLine("Площадь круга: ", S);
+    return S;
+}
+    }
+}
